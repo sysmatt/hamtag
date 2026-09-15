614b6c0b3f8d442ccf6dcb43f4bb151d09d47a42
Matt Hoskins <matthoskins@gmail.com>
2026-09-15 16:32:18 -0400

    added helper script

--- SCARC-meeting.sh
+++ SCARC-meeting.sh
@@ -1,4 +1,7 @@
 #!/bin/bash 
 
 cd `dirname $0`
-./hamtag --printer /dev/usb/lp0 --lang tspl --label 4x2 --jokes ../DadJokeGenerator_LA_FP/data/dad-a-base.csv --banner "Sussex County Amateur Radio Club"  --note "Guest" --note-lookup ~/Documents/HAM/SCARC.membership.callsigns.hamtag.note.csv    ${1:- --gui}
+OPT=${1:- --gui}
+shift
+
+./hamtag --printer /dev/usb/lp0 --lang tspl --label 4x2 --jokes ~/Documents/dad-a-base.csv --banner "Sussex County Amateur Radio Club"  --note "Guest" --note-lookup ~/Documents/HAM/SCARC.membership.callsigns.hamtag.note.csv  $OPT $@
